2cda15d2a4bccc80da1623d5eb503a03b19ab400
jisoo <doh_0112@naver.com>
2022-01-05 02:47:28 +0900

    필요없는 주석 제거

--- JS/index.js
+++ JS/index.js
@@ -9,7 +9,6 @@ const USERNAME_KEY = 'username';
 function onLoginSubmit(event) {
     event.preventDefault();
     location.href = 'main.html';
-    // loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
     localStorage.setItem(USERNAME_KEY, userName);
     paintGreeting(userName);
@@ -17,21 +16,20 @@ function onLoginSubmit(event) {
 
 function paintGreeting(username) {
     greeting.innerText = `hello ${username}`;
-    // greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
     // show the form
-    location.href = 'index.html';
     loginForm.addEventListener('submit', onLoginSubmit);
 } else {
     // show the greeting
     if (window.location.pathname === '/index.html') {
         location.href = 'main.html';
-    }
-    paintGreeting(savedUsername);   
+    } else {
+        paintGreeting(savedUsername); 
+    }  
 }
 
 
