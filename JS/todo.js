5e4b9798d0be1ed8f41a503af1890149b57180c0
jisu <93499095+amisu1203@users.noreply.github.com>
2022-01-04 10:35:52 +0900

    todoSubmit 함수 생성

--- JS/todo.js
+++ JS/todo.js
@@ -1,3 +1,10 @@
 const todoForm = document.getElementById('todo-form');
+const todoInp = todoForm.querySelector('input');
 const todoList = document.getElementById('todo-list');
 
+function todoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInp.value;
+    todoInp.value = '';
+}
+todoForm.addEventListener('submit', todoSubmit);
