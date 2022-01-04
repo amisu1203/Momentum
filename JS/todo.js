400188c488a110f722ac44d4b2ee70e524e36676
jisu <93499095+amisu1203@users.noreply.github.com>
2022-01-04 13:53:48 +0900

    deleteTodo 함수 생성

--- JS/todo.js
+++ JS/todo.js
@@ -2,12 +2,18 @@ const todoForm = document.getElementById('todo-form');
 const todoInp = todoForm.querySelector('input');
 const todoList = document.getElementById('todo-list');
 
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo) {
     const li = document.createElement('li');
     const span = document.createElement('span');
     span.innerText = newTodo;
     const btn = document.createElement('button');
     btn.innerText = '❌';
+    btn.addEventListener('click', deleteTodo);
 
     li.appendChild(span);
     li.appendChild(btn);
