70293f83453b757e9580affad5b9cc6813ac4e6f
jisu <93499095+amisu1203@users.noreply.github.com>
2022-01-04 11:14:54 +0900

    paintTodo 함수 생성

--- JS/todo.js
+++ JS/todo.js
@@ -2,9 +2,22 @@ const todoForm = document.getElementById('todo-form');
 const todoInp = todoForm.querySelector('input');
 const todoList = document.getElementById('todo-list');
 
-function todoSubmit(event) {
+function paintTodo(newTodo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = newTodo;
+    const btn = document.createElement('button');
+    btn.innerText = '❌';
+
+    li.appendChild(span);
+    li.appendChild(btn);
+    todoList.appendChild(li);
+}
+
+function submitTodo(event) {
     event.preventDefault();
     const newTodo = todoInp.value;
     todoInp.value = '';
+    paintTodo(newTodo);
 }
-todoForm.addEventListener('submit', todoSubmit);
+todoForm.addEventListener('submit', submitTodo);
